734af2806cc46f21ab7e008726401432239159e6
Nico Rutishauser <nr@hotmail.ch>
2014-03-29 11:19:36 +0100

    Added result table
    Signed-off-by: Nico Rutishauser <nr@hotmail.ch>

--- ch.uzh.ifi.seal.ase.group3/db/schema.sql
+++ ch.uzh.ifi.seal.ase.group3/db/schema.sql
@@ -16,3 +16,23 @@ create table tweet (
 create index id_idx on tweet(id);
 create index text_idx on tweet(text);
 create index preprocessed_idx on tweet(preprocessed);
+
+-- ----------------------------------------------------------------------------
+-- result
+-- ----------------------------------------------------------------------------
+create table result (
+	id serial primary key NOT NULL,
+
+	-- the term searched for
+	query text NOT NULL,
+	
+	-- the score of the result
+	score decimal NOT NULL,
+	
+	-- date when the result has been computed
+	computed_at date NOT NULL default CURRENT_DATE	
+);
+
+create index res_id_idx on result(id);
+create index res_score_idx on result(score);
+	
